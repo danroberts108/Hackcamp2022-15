4323a7da1c531fa747be9c0121595d8df8920b53
danroberts108 <d.roberts15@edu.salford.ac.uk>
2022-01-11 20:43:48 +0000

    Added Risk model and start of csv parsing code.

---
+++ src/Models/CsvParsing.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("Risk.php");
+
+class CsvParsing {
+    public function getRiskFromCsv($file) {
+        $data = array();
+        if (($handle = fopen($file, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $risk = new Risk($data[0], $data[1], $data[2], $data[3]);
+                $data[] = $risk;
+            }
+        }
+        return $data;
+    }
+
+    public function getCsvFromRisk($data) {
+
+    }
+}
---
+++ src/Models/Risk.php
@@ -0,0 +1,93 @@
+<?php
+
+class Risk {
+    protected $id, $lon, $lat, $distance, $district;
+
+    public function __construct($lat, $lon, $distance, $district) {
+        $this->lon = $lon;
+        $this->lat = $lat;
+        $this->distance = $distance;
+        $this->district = $district;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLon()
+    {
+        return $this->lon;
+    }
+
+    /**
+     * @param mixed $lon
+     */
+    public function setLon($lon): void
+    {
+        $this->lon = $lon;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * @param mixed $lat
+     */
+    public function setLat($lat): void
+    {
+        $this->lat = $lat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDistance()
+    {
+        return $this->distance;
+    }
+
+    /**
+     * @param mixed $distance
+     */
+    public function setDistance($distance): void
+    {
+        $this->distance = $distance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDistrict()
+    {
+        return $this->district;
+    }
+
+    /**
+     * @param mixed $district
+     */
+    public function setDistrict($district): void
+    {
+        $this->district = $district;
+    }
+
+}
