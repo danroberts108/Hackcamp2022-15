3937be9008a4ed30c7c73a23c80625e8a65b91c4
NJAspinall <97522482+NJAspinall@users.noreply.github.com>
2022-01-12 11:16:03 +0000

    Add files via upload

---
+++ src/upload.php
@@ -0,0 +1,32 @@
+<?php
+require_once('Models/Risks.php');
+require_once('Models/CsvParsing.php');
+$view = new stdClass();
+$view->pageTitle = 'Upload';
+
+$riskSet = new Risks();
+$parse = new CsvParsing();
+//$riskSet->testConnection();
+
+//The form that lets the user upload a csv
+if(isset($_POST['submit'])) {
+    if(isset($_FILES['chosenFile'])) {
+        try {
+            $uploadDir = "../Uploads/";
+            $uploadFile = $uploadDir.basename($_FILES['chosenFile']['name']);
+
+            if(is_uploaded_file($_FILES['chosenFile']['tmp_name'])) { //if file was uploaded
+                //echo "[OPENING CSV]";
+                $parse->getRiskFromCsv($_FILES['chosenFile']['tmp_name']);
+            }
+        }
+        catch(Exception $e) {
+
+        }
+    }
+
+}
+
+
+
+require_once('Views/upload.phtml');
