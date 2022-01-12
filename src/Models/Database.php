70a4e7e3b74949bdf4fe25217d2da74d22942777
danroberts <d.roberts15@edu.salford.ac.uk>
2022-01-12 13:11:50 +0000

    Fixed file references in submitdatatest.php

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -7,7 +7,7 @@ class Database
         try {
             $username = "hc22-15";
             $password = "ssf7cP0HNHKqMcBf";
-            $databaseHandler = new PDO('mysql:host=poseidon.salford.ac.uk;dbname=hc22_15', $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            $databaseHandler = new PDO('mysql:host=hc22-15.poseidon.salford.ac.uk;dbname=hc22_15', $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
             return $databaseHandler;
         }
         catch(PDOException $e) {
